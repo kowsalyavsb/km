065d0280dbf65d851ddfccb218c6db1abf8533b1
kowsalyavsb <31506020+kowsalyavsb@users.noreply.github.com>
2018-02-01 14:40:40 +0530

    Update fibi.c

--- fibi.c
+++ fibi.c
@@ -3,13 +3,14 @@ int main()
 {
 int i,n,k=0,s=1,t;
 scanf("%d",&n);
-printf("\n%d\t%d",a,b);
+printf("%d\n%d\n",k,s);
 for(i=0;i<n;i++)
 {
 t=k+s;
-k=s
+k=s;
 s=t;
-printf("%d\n",b);
+printf("%d\n",s);
 }
 return 0;
 }
+
